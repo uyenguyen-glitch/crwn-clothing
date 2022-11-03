9fb0070aad23555a45ae93dcbdd74127e286b329
uyenguyen-glitch <uyenguyen2000@gmail.com>
2022-11-03 20:28:26 +0700

    finished reducer part_2

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -3,6 +3,7 @@ import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
 } from "../utils/firebase/firebase.utils";
+import { createAction } from "../utils/reducer/reducer.utils";
 // as the actual value you want to access
 export const UserContext = createContext({
   currentUser: null,
@@ -14,8 +15,6 @@ export const USER_ACTION_TYPE = {
 };
 
 const userReducer = (state, action) => {
-  console.log("dispatched");
-  console.log(action);
   const { type, payload } = action;
 
   switch (type) {
@@ -42,7 +41,7 @@ export const UserProvider = ({ children }) => {
   const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
 
   const setCurrentUser = (user) => {
-    dispatch({ type: USER_ACTION_TYPE.SET_CURRENT_USER, payload: user });
+    dispatch(createAction(USER_ACTION_TYPE.SET_CURRENT_USER, user));
   };
 
   const value = { currentUser, setCurrentUser };
